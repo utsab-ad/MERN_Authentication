93a4f0ea973e9b1a3092ea1c582352cc798f1455
utsab-ad <utasbadhikari075@gmail.com>
2025-04-28 10:05:30 -0600

    mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
 const PORT = 3030
 
@@ -8,14 +9,16 @@ app.get("/", (req, res) => {
     res.send("Hello there, this is live");
 })
 
+mongoose.connect("mongodb+srv://utsabadhikari075:VWOCc3ddaY97to6i@cluster0.oh2zftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 app.listen(PORT, ()=> {
     console.log(`The server is running on the http://localhost:${PORT}`);
 })
 
 // mongodb atlas connection!!
 
-// {mongodb+srv://utsabadhikari075:VWOCc3ddaY97to6i@cluster0.oh2zftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// }
+mongodb+srv://utsabadhikari075:VWOCc3ddaY97to6i@cluster0.oh2zftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 
 
 //VWOCc3ddaY97to6i
