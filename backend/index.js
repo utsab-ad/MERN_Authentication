aa84ead0d0bc570f196f9663b24148babe2048a7
utsab-ad <utasbadhikari075@gmail.com>
2025-04-28 10:03:18 -0600

    Mongo Atlas created

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, ()=> {
     console.log(`The server is running on the http://localhost:${PORT}`);
 })
+
+// mongodb atlas connection!!
+
+// {mongodb+srv://utsabadhikari075:VWOCc3ddaY97to6i@cluster0.oh2zftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// }
+
+
+//VWOCc3ddaY97to6i
